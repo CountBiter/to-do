f2818697c2dd93ce0d969b4aee71d4df97952c98
Никита Чёрный <countbiter1@gmail.com>
2022-10-13 17:10:39 +0600

    To-Do

--- 99_diploma/index.js
+++ 99_diploma/index.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 
 const express = require("express");
-const session = require("express-session");
+const session = require("cookie-session");
 const passport = require("passport");
 const strategies = require("./strategies/passport-strategies.js");
 
